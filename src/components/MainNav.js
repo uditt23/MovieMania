b1da4b6a960849f0a783469cb3ec98de45a933bf
Udit Hinger <uditjhinger@gmail.com>
2023-06-04 13:31:30 +0530

    First Commit

---
+++ src/components/MainNav.js
@@ -0,0 +1,77 @@
+import React, { useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import TvIcon from "@material-ui/icons/Tv";
+import MovieIcon from "@material-ui/icons/Movie";
+import AddIcon from '@material-ui/icons/Add';
+import SearchIcon from "@material-ui/icons/Search";
+import WhatshotIcon from "@material-ui/icons/Whatshot";
+import { useHistory } from "react-router-dom";
+
+const useStyles = makeStyles({
+  root: {
+    width: "100%",
+    position: "fixed",
+    bottom: 0,
+    backgroundColor: "#2d313a",
+    zIndex: 100,
+  },
+});
+
+export default function SimpleBottomNavigation() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (value === 0) {
+      history.push("/");
+    } else if (value === 1) {
+      history.push("/movies");
+    } else if (value === 2) {
+      history.push("/series");
+    } else if (value === 3) {
+      history.push("/search");
+    } else if (value === 4) {
+      history.push("/addmovie");
+    }
+  }, [value, history]);
+
+  return (
+    <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+      showLabels
+      className={classes.root}
+    >
+      <BottomNavigationAction
+        style={{ color: "white" }}
+        label="Trending"
+        icon={<WhatshotIcon />}
+      />
+      <BottomNavigationAction
+        style={{ color: "white" }}
+        label="Movies"
+        icon={<MovieIcon />}
+      />
+      <BottomNavigationAction
+        style={{ color: "white" }}
+        label="TV Series"
+        icon={<TvIcon />}
+      />
+      <BottomNavigationAction
+        style={{ color: "white" }}
+        label="Search"
+        icon={<SearchIcon />}
+      />
+      <BottomNavigationAction
+        style={{ color: "white" }}
+        label="Add"
+        icon={<AddIcon />}
+      />
+    </BottomNavigation>
+  );
+}
